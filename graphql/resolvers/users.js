88cedf3aac0931d0d5bb36055e0615975b64749e
Andrei Schneider Majada <andreimajada@gmail.com>
2021-01-22 20:51:24 -0300

    :truck: Adjusting files

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,7 +2,7 @@ const { UserInputError } = require("apollo-server");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../../models/User");
+const User = require("../../models/user");
 const { SECRET_KEY } = require("../../config");
 const {
   validateRegisterInput,
