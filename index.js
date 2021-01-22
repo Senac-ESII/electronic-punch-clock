23ba3f0474695bb33ec0b78a57999a7a7b041944
Andrei Schneider Majada <andreimajada@gmail.com>
2021-01-22 14:59:36 -0300

    :sparkles: Create user working fine

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql, UserInputError } = require("apollo-server");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
+const User = require("./models/User");
 const { MONGODB, PORT } = require("./config");
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    createdAt: String!
+  }
+  type Mutation {
+    register(username: String!, email: String!, password: String!): User
+  }
   type Query {
-    sayHi: String!
+    register: User!
   }
 `;
 const resolvers = {
-  Query: {
-    sayHi: () => "Testing types!",
+  Mutation: {
+    async register(_, { username, email, password }, context, info) {
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username already taken!", {
+          errors: {
+            username: "This username ir taken",
+          },
+        });
+      }
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        username,
+        email,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+      return {
+        ...res._doc,
+        id: res._id,
+      };
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => ({ req }),
 });
 
 mongoose
