7537ce94ea186fb9444f3dd455dc3368e2745e5e
Andrei Schneider Majada <andreimajada@gmail.com>
2021-01-23 22:02:26 -0300

    :sparkles: Login page working

--- register/src/App.js
+++ register/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/authRoute";
+
+import Login from "./pages/Login";
 
 function App() {
   return (
-    
+    <AuthProvider>
+      <Router>
+        <Container>
+          <AuthRoute exact path="/login" component={Login} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
